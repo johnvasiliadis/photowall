0e926ef715c3218b31466c215bc7b5b6eebc369c
senor9k <johnvasiliadis@me.com>
2018-07-05 14:22:08 -0400

    Fix header

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -24,7 +24,7 @@ class Main extends Component {
         return (
             <div>
                 {/* <h1><Link to="/">Photowall</Link></h1> */}
-                <h1><Link to="/">Might Right</Link></h1>
+                <h1><Link to="/">Photowall</Link></h1>
                 <Route exact path="/" render={() => (
                     <div>
                         <PhotoWall {...this.props} />
